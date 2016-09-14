5f505edc21e37d70bc42a894ae05db305875150f
nedilio <izquierdonelson@gmail.com>
2016-09-14 14:30:32 -0400

    Estilos de contact form

--- template-contacto.php
+++ template-contacto.php
@@ -19,7 +19,7 @@
 	<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d11446.400347100684!2d5.2699233439737565!3d44.17410155574725!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12ca645aaf6f7367%3A0xd3f24ac5c0a1bcd!2sMt+Ventoux!5e0!3m2!1sen!2sve!4v1471977061249" width="100%" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
 </div>
 
-<div class="container">
+<div class="container contactos">
 	<div class="row">
 		<div class="col-sm-6">
 			<p>
@@ -38,7 +38,7 @@
 			</address>
 		</div>
 		<div class="col-sm-6">
-
+			<h2 class="envianos">ENVIANOS UN MENSAJE</h2>
 <?php echo do_shortcode('[contact-form-7 id="96" title="Contact form 1"]'); ?>
 
 		</div>
