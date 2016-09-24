00649f48eb76985602fc2d00b3a54baa979b0447
nedilio <izquierdonelson@gmail.com>
2016-09-23 21:51:27 -0400

    mejorando codigo de fuente open sans

--- templates/head.php
+++ templates/head.php
@@ -3,6 +3,7 @@
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link href="https://file.myfontastic.com/gKs7rXhZ2Js32K26nxDoRL/icons.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i" rel="stylesheet">
   <?php wp_head(); ?>
 </head>
 		
