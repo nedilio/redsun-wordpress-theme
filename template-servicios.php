29dc63fba05c2ab70cc69b2eee28ebac1a414907
nedilio <izquierdonelson@gmail.com>
2016-10-24 18:14:11 -0300

    altos de los tabs de servicio1s y slider de otros servicios

--- template-servicios.php
+++ template-servicios.php
@@ -78,7 +78,7 @@ $count++;
                 <div id="servicio<?php echo $count;?>" class="tab-pane fade <?php echo $class;?>">
                   <?php echo $cajasflechas; ?>
                   <div class="row">
-                    <div class="col-sm-10 col-sm-offset-1">
+                    <div class="col-sm-10 col-sm-offset-1 tab-servicio">
                   <h2 class="text-uppercase text-center">
                     <?php echo get_the_title();?>
                   </h2>
